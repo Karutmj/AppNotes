70d9df5e7a579cee91b99ea6b16f1e51e9623e85
Szymon Maciołek <emaciole15072@gmail.com>
2023-12-26 22:55:40 +0100

    Pierwszy commit

--- script.js
+++ script.js
@@ -5,28 +5,30 @@ function addNote() {
     // Sprawdź, czy tekst nie jest pusty
     if (noteText.trim() !== "") {
         // Stwórz nowy element notatki
-        var noteElement = document.createElement("div");
+        var noteElement = document.createElement("p");
         noteElement.className = "note";
         noteElement.textContent = noteText;
-        var newNoteElement = document.createElement("div");
-        noteElement.className="NewNote"
-        newNoteElement.textContent = "New Note: ";
-        document.getElementById("notes-container").appendChild(newNoteElement);
+
+        // Dodaj krzyżyk do usuwania notatki
+        var deleteButton = document.createElement("h6");
+        deleteButton.className = "delete-button";
+        deleteButton.textContent = "Clear";
+        deleteButton.addEventListener("click", function () {
+            // Funkcja obsługująca kliknięcie na krzyżyk - usuń notatkę
+            noteElement.parentNode.removeChild(noteElement);
+        });
+
+        // Dodaj krzyżyk do elementu notatki
+        noteElement.appendChild(deleteButton);
 
         // Dodaj notatkę do kontenera
         document.getElementById("notes-container").appendChild(noteElement);
 
         // Wyczyść pole input
         document.getElementById("input_field").value = "";
-       
     }
 }
-document.addEventListener("DOMContentLoaded", function () {
-    // Wykryj zdarzenie załadowania strony
-    window.onload = function () {
-        // Ukryj animację ładowania po załadowaniu strony
-        document.getElementById("loader-container").style.display = "none";
-        // Przywróć widoczność paska przewijania
-        document.body.style.overflow = "visible";
-    };
-});
+
+function resetMe() {
+    document.getElementById("notes-container").innerHTML = "";
+}
